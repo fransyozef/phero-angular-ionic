1369e7dd99362c2340071f678a35c3ceebfc9bfd
Fransjo Leihitu <fransyozef@fransjos-mbp.speedport.ip>
2023-04-09 15:54:14 +0200

    update to Phero 0.10.x

--- api/phero-manifest.d.ts
+++ api/phero-manifest.d.ts
@@ -1,34 +1,30 @@
-export declare namespace domain {
-    namespace v_1_0_0 {
-        enum ArticleComponentType {
-            Paragraph = "paragraph",
-            Image = "image",
-            Quote = "quote"
-        }
-        type ArticleComponent = {
-            type: ArticleComponentType.Paragraph;
-            content: string;
-        } | {
-            type: ArticleComponentType.Image;
-            url: string;
-        } | {
-            type: ArticleComponentType.Quote;
-            quote: string;
-            source: string;
-        };
-        interface Article {
-            id: string;
-            title: string;
-            components: domain.v_1_0_0.ArticleComponent[];
-        }
-        class ArticleNotFoundError extends Error {
-            constructor(message: string);
-        }
-    }
+export interface Article {
+    id: string;
+    title: string;
+    components: ArticleComponent[];
 }
-export declare namespace articleService {
-    namespace v_1_0_0 {
-        function getArticle(id: string): Promise<domain.v_1_0_0.Article>;
-        function listArticles(): Promise<domain.v_1_0_0.Article[]>;
-    }
+export type ArticleComponent = {
+    type: ArticleComponentType.Paragraph;
+    content: string;
+} | {
+    type: ArticleComponentType.Image;
+    url: string;
+} | {
+    type: ArticleComponentType.Quote;
+    quote: string;
+    source: string;
+};
+export enum ArticleComponentType {
+    Paragraph = "paragraph",
+    Image = "image",
+    Quote = "quote"
+}
+export class ArticleNotFoundError extends Error {
+    constructor(message: string);
+}
+export abstract class PheroService<TContext = {}> {
+}
+export class articleService extends PheroService {
+    getArticle(id: string): Promise<Article>;
+    listArticles(): Promise<Article[]>;
 }
